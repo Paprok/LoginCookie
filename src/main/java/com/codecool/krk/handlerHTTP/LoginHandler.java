57c3b97237498be3ccaf5dc77491a7c4988b0741
Paprok <rafal.becla@gmail.com>
2018-12-27 10:04:17 +0100

    created LoginHandler

--- src/main/java/com/codecool/krk/handlerHTTP/LoginHandler.java
+++ src/main/java/com/codecool/krk/handlerHTTP/LoginHandler.java
@@ -1,5 +1,20 @@
-package com.codecool.krk;
+package com.codecool.krk.handlerHTTP;
 
-public class LoginHandler {
+import com.codecool.krk.dao.DAOLogin;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
 
+import java.io.IOException;
+
+public class LoginHandler implements HttpHandler {
+    private DAOLogin daoLogin;
+
+    public LoginHandler(DAOLogin daoLogin) {
+        this.daoLogin = daoLogin;
+    }
+
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+
+    }
 }
