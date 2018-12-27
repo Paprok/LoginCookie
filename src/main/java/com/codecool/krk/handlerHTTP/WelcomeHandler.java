77f7e22e56483ce2d1d0cb5a012c607dc6070f9a
Paprok <rafal.becla@gmail.com>
2018-12-27 12:47:14 +0100

    implemented welcome page handler, creates get response, no user validation

--- src/main/java/com/codecool/krk/handlerHTTP/WelcomeHandler.java
+++ src/main/java/com/codecool/krk/handlerHTTP/WelcomeHandler.java
@@ -6,10 +6,15 @@ import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
+import java.io.OutputStream;
 
 public class WelcomeHandler implements HttpHandler {
     private DAOLogin daoLogin;
     private CookieHelper cookieHelper;
+    private HttpExchange httpExchange;
+    private String response;
+    private int code;
+    private String location;
 
     public WelcomeHandler(DAOLogin daoLogin, CookieHelper cookieHelper) {
         this.daoLogin = daoLogin;
@@ -18,6 +23,31 @@ public class WelcomeHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
+        this.httpExchange = httpExchange;
+        resetFields();
+        handleRequestGET();
+        sendExchange();
+    }
 
+    private void resetFields() {
+        this.response = "";
+        this.location = "/";
+        this.code = 404;
+    }
+
+    private void handleRequestGET(){
+        this.response = "<html><head></head><body>" +
+                "<h> Welcome </h>" +
+                "</body></html>";
+        this.code = 200;
+        this.location = "/welcome";
+    }
+
+    private void sendExchange() throws IOException{
+        this.httpExchange.getResponseHeaders().add("Location", this.location);
+        this.httpExchange.sendResponseHeaders(this.code, this.response.length());
+        OutputStream os = this.httpExchange.getResponseBody();
+        os.write(this.response.getBytes());
+        os.close();
     }
 }
