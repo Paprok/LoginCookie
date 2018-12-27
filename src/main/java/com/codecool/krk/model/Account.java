cfbf8800084428dda2728d372d27471525f82b15
Paprok <rafal.becla@gmail.com>
2018-12-27 12:28:37 +0100

    changed user_id type to int

--- src/main/java/com/codecool/krk/model/Account.java
+++ src/main/java/com/codecool/krk/model/Account.java
@@ -1,4 +1,43 @@
 package com.codecool.krk.model;
 
 public class Account {
+    private String name;
+    private String password;
+    private int user_id;
+    private String session_id;
+
+    public Account() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getSession_id() {
+        return session_id;
+    }
+
+    public void setSession_id(String session_id) {
+        this.session_id = session_id;
+    }
 }
